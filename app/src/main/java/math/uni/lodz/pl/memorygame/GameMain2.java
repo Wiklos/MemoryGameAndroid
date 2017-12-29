6e4f4fffa7725f3664469aa20f68e87712a197fd
Wiklos <wiklos24@interia.eu>
2017-12-29 16:35:13 +0100

    Add logic of memory game
    Add toast when get a matching images.
    Need to fix when clicked the same image twice.
    Need to do random placing of images.

--- app/src/main/java/math/uni/lodz/pl/memorygame/GameMain2.java
+++ app/src/main/java/math/uni/lodz/pl/memorygame/GameMain2.java
@@ -90,113 +90,373 @@ public class GameMain2 extends AppCompatActivity {
     public boolean swap9 = true;
     public boolean swap10 = true;
 
+    public int clickCounter = 0;
+    public int tag1 = 0;
+    public int tag2 = 0;
+    public boolean gotTag1 = false;
     public  void change1(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton);
-        if (swap1) {
-            //i.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() +
-            //        File.separator + "MemoryGamePictures" + File.separator + "Noga" + "/pic1.jpg"));
-            i.setImageBitmap(BitmapFactory.decodeFile(path1));
-            swap1 = false;
-        } else {
-            swap1 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton);
+            if (swap1) {
+                //i.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() +
+                //        File.separator + "MemoryGamePictures" + File.separator + "Noga" + "/pic1.jpg"));
+                i.setImageBitmap(BitmapFactory.decodeFile(path1));
+                swap1 = false;
+            } else {
+                swap1 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 1;
+                gotTag1 = true;
+            }
+
         }
+        if(clickCounter==2)
+        {
+            tag2=1;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
-    public void change2(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton2);
-        if (swap2) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path2));
-            swap2 = false;
-        } else {
-            swap2 = true;
-            i.setImageResource(R.drawable.q_mark);
+    public  void change2(View view) {
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton2);
+            if (swap2) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path2));
+                swap2 = false;
+            } else {
+                swap2 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 1;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=1;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change3(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton3);
-        if (swap3) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path3));
-            swap3 = false;
-        } else {
-            swap3 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton3);
+            if (swap3) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path3));
+                swap3 = false;
+            } else {
+                swap3 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 2;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=2;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change4(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton4);
-        if (swap4) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path4));
-            swap4 = false;
-        } else {
-            swap4 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton4);
+            if (swap4) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path4));
+                swap4 = false;
+            } else {
+                swap4 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 2;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=2;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change5(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton5);
-        if (swap5) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path5));
-            swap5 = false;
-        } else {
-            swap5 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton5);
+            if (swap5) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path5));
+                swap5 = false;
+            } else {
+                swap5 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 3;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=3;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change6(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton6);
-        if (swap6) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path6));
-            swap6 = false;
-        } else {
-            swap6 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton6);
+            if (swap6) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path6));
+                swap6 = false;
+            } else {
+                swap6 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 3;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=3;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change7(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton7);
-        if (swap7) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path7));
-            swap7 = false;
-        } else {
-            swap7 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton7);
+            if (swap7) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path7));
+                swap7 = false;
+            } else {
+                swap7 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 4;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=4;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change8(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton8);
-        if (swap8) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path8));
-            swap8 = false;
-        } else {
-            swap8 = true;
-            i.setImageResource(R.drawable.q_mark);
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton8);
+            if (swap8) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path8));
+                swap8 = false;
+            } else {
+                swap8 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 4;
+                gotTag1 = true;
+            }
         }
+        if(clickCounter==2)
+        {
+            tag2=4;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
     }
     public  void change9(View view) {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton9);
-        if (swap9) {
-            i.setImageBitmap(BitmapFactory.decodeFile(path9));
-            swap9 = false;
-        } else {
-            swap9 = true;
-            i.setImageResource(R.drawable.q_mark);
-        }
-    }
-    public  void change10(View view)
-    {
-        ImageButton i = (ImageButton) findViewById(R.id.imageButton10);
-        if (swap10)
+        if(clickCounter==0 || clickCounter==1)
         {
-            i.setImageBitmap(BitmapFactory.decodeFile(path10));
-            swap10=false;
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton9);
+            if (swap9) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path9));
+                swap9 = false;
+            } else {
+                swap9 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 5;
+                gotTag1 = true;
+            }
         }
-        else
+        if(clickCounter==2)
         {
-            swap10=true;
-            i.setImageResource(R.drawable.q_mark);
+            tag2=5;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
         }
-    }
 
+    }
+    public  void change10(View view) {
+        if(clickCounter==0 || clickCounter==1)
+        {
+            ImageButton i = (ImageButton) findViewById(R.id.imageButton10);
+            if (swap10) {
+                i.setImageBitmap(BitmapFactory.decodeFile(path10));
+                swap10 = false;
+            } else {
+                swap10 = true;
+                i.setImageResource(R.drawable.q_mark);
+            }
+            clickCounter++;
+            if(!gotTag1)
+            {
+                tag1= 5;
+                gotTag1 = true;
+            }
+        }
+        if(clickCounter==2)
+        {
+            tag2=5;
+            if(tag1==tag2)
+            {
+                Toast.makeText(getApplication(),"Its a match!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+            else
+            {
+                Toast.makeText(getApplication(),"Try again!", Toast.LENGTH_SHORT).show();
+                clickCounter=0;
+                gotTag1=false;
+            }
+        }
+
+    }
 
 
 }
